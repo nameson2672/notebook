cf43e251a358cf0178904ccb99915a123496c637
Nameson Gaudel <namesongaudel.ng@gmail.com>
2022-06-25 17:02:47 +0545

    consloe added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import child_process from 'child_process';
 // getting port  
 const port = process.env.PORT;
 
+
 // Running the serve commadn on vli and passing the port from heroku
 child_process.exec(`mdbook serve -p ${port} -n 127.0.0.1`,(e, stdout, stderr)=>{
     console.log(stdout);
